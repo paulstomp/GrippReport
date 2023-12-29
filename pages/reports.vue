0aa73cd476244b1cab2e7606dfd341087c12e19c
paulstomp <64829164+paulstomp@users.noreply.github.com>
2023-12-29 14:01:09 +0100

    styling

--- pages/reports.vue
+++ pages/reports.vue
@@ -1,21 +1,21 @@
 <template>
     <ClientOnly fallback-tag="span" fallback="Loading comments...">
-      <div class="grid-1">
+        <div class="grid-1">
 
-        <!-- Hour report -->
+            <!-- Hour report -->
 
-        <div class="card light-dark shadow">
+            <div class="card light-dark shadow">
 
-            <h1>Project hours</h1>
+                <h1>Project hours</h1>
 
-            <p>booked gap = booked - planned until today</p>
-            <p>budgeted left = budgeted - booked</p>
-            <p>balance = budgeted left - planned from today</p>
+                <p>booked gap = booked - planned until today</p>
+                <p>budgeted left = budgeted - booked</p>
+                <p>balance = budgeted left - planned from today</p>
 
-            <DataView :data=data :maxLength=20></DataView>
+                <DataView :data=data :maxLength=20></DataView>
+            </div>
+
         </div>
-
-        </div> 
     </ClientOnly>
 </template>
 
