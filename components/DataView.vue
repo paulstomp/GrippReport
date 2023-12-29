6591f4b9a0df8f92b991e23ec5d45a04894f603a
paulstomp <64829164+paulstomp@users.noreply.github.com>
2023-12-29 09:24:22 +0100

    sorting

--- components/DataView.vue
+++ components/DataView.vue
@@ -5,15 +5,18 @@
 
             <!-- Header -->
 
-            <tr style="font-weight: bold">
-                <td v-for="(field, index) in Object.keys(data[0])" :key=index style="padding: 5px;">
-                    {{ field.replaceAll('_', ' ') }}
+            <tr height="30px" style="font-weight: bold">
+                <td v-for="(field, index) in Object.keys(data[0])" :key=index style="padding: 5px;" nowrap>
+                    <div @click="sort(field)" style="cursor: pointer;">
+                        {{ field.replaceAll('_', ' ') }}
+                        <span style="width: 20px; display: inline-block;">{{ getSortIcon(field) }}</span>
+                    </div>
                 </td>
             </tr>
 
             <!-- Data -->
 
-            <tr v-for="(row, index) in data" :key=index class="view">
+            <tr v-for="(row, index) in tableData" :key=index class="view">
                 <td v-for="(field, index) in Object.keys(data[0])" :key=index style="padding: 5px;">
                     {{ prettyValue(row[field]) }}
                 </td>
@@ -36,6 +39,39 @@
 <script setup lang="ts">
 
     const props = defineProps(['data', 'maxLength']);
+
+    var tableData = ref(props.data);
+    var sortField = ref('');
+    var sortOrderAsc = ref(true);
+
+    function sort(field: string) {
+        if (field == sortField.value) {
+            sortOrderAsc.value = !sortOrderAsc.value;
+        } else {
+            sortField.value = field;
+            sortOrderAsc.value = true;
+        }
+
+        tableData.value.sort((rowA: any, rowB: any) => compare(rowA[field], rowB[field]));
+    }
+
+    function compare(a: any, b: any) {
+        var difference: number;
+        const aNum = a ? parseFloat(a) : 0;
+        const bNum = b ? parseFloat(b) : 0;
+        if (!isNaN(aNum) && !isNaN(bNum)) {
+            difference = aNum - bNum;
+        } else {
+            difference = a.localeCompare(b);
+        }
+        return sortOrderAsc.value ? difference : -difference;
+    }
+
+    function getSortIcon(field: string) {
+        if (field == sortField.value) {
+            return sortOrderAsc.value ? '▼' : '▲';
+        }
+    }
 
     function prettyValue(value: any) {
 
