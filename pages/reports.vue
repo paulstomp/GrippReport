799c5c60fd602337453000d7a4486864eb31fa2e
Paul Stomp <paul.stomp@gmail.com>
2024-03-21 16:41:04 +0100

    reports

--- pages/reports.vue
+++ pages/reports.vue
@@ -21,7 +21,7 @@
 
 <script lang="ts" setup>
 
-  const sql = `select company_name as company, name as project, hours_offered as budgeted,
+  const sql = `select company_name as company, name as project, number, hours_offered as budgeted,
     hours_planned as planned, hours_planned_until_today as planned_until_today,
     hours_booked as booked, hours_booked_gap as booked_gap,
     hours_left as budgeted_left, hours_planned_from_today as planned_from_today,
